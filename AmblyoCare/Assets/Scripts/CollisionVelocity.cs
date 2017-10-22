13521f0587062bab166154997ff05cf99ccaab46
Ada <prosa100@gmail.com>
2017-10-22 10:10:57 -0500

    initial full commit

---
+++ AmblyoCare/Assets/Scripts/CollisionVelocity.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionVelocity : MonoBehaviour {
+
+    void OnCollisionEnter(Collision c)
+    {
+        string name = gameObject.name;
+        gameObject.GetComponent<Rigidbody>().velocity = -gameObject.GetComponent<Rigidbody>().velocity;
+
+    }
+}
